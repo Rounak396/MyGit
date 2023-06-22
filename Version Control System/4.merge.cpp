d56174b8a38c32a935511df8c5f59a8b1e9ae9e3
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-06-22 05:45:41 +0530

    Update 4.merge.cpp

---
+++ Version Control System/4.merge.cpp
@@ -0,0 +1,24 @@
+namespace mygitmerge{
+    #include <iostream>
+    #include <string>
+    #include <unistd.h>
+    #include <stdio.h>
+    #include <limits.h>
+    #include <dirent.h>
+    #include <sys/types.h>
+    #include <fcntl.h>
+    #include <errno.h>
+    #include <time.h>
+    #include <vector>
+    #include <map>
+    #include <fstream>
+    #include <algorithm>
+    #include <sys/stat.h>
+    #include <sstream>
+    #include <iterator>
+
+    using namespace std;
+
+    vector<string> get_files(string);
+    
+}
