e8119783b574f539d81b4467721cd40ba7e4f4e6
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-06-22 06:02:30 +0530

    Update 4.merge.cpp

--- Version Control System/4.merge.cpp
+++ Version Control System/4.merge.cpp
@@ -19,6 +19,54 @@ namespace mygitmerge{
 
     using namespace std;
 
-    vector<string> get_files(string);
-    
+    vector<string> merge_get_files(string);
+    string get_cwd();
+    string get_sha(string);
+
+    int merge(){
+        // removes files from current working directory and move  files from push folder to current working directory
+        // (if there is a conflict, then it will not remove the file from current working directory) (also
+        // removes file from push after copying)
+
+        // get the current working directory
+        string cwd = get_cwd();
+
+        // removing files from cwd
+        vector<string> file_in_cwd = merge_get_files(cwd);
+        for(int i=0;i<file_in_cwd.size();i++){
+            string file_path = cwd + "/" + file_in_cwd[i];
+            remove(file_path.c_str());
+        }
+
+
+        // moving files from push folder to cwd
+        string push_path = cwd + "/.mygit/push";
+        vector<string> file_in_push = merge_get_files(push_path);
+        for(int i=0;i<file_in_push.size();i++){
+            string file_path = push_path + "/" + file_in_push[i];
+            string new_file_path = cwd + "/" + file_in_push[i];
+            rename(file_path.c_str(),new_file_path.c_str());
+            cout<< "Moving file" << file_in_push[i] << " from push folder to current working directory" << endl;
+
+            ifstream ifile(file_path.c_str());
+
+            string s;
+            ofstream ofile(new_file_path.c_str());
+
+            ifile.open(file_path,ios::in);
+            ofile.open(new_file_path,ios::out);
+
+            while(ifile){
+                getline(ifile , s);
+                ofile << s << endl;
+            }
+
+            remove(file_path.c_str());
+
+            ifile.close();
+            ofile.close();
+        }
+        return 0;
+    }
+
 }
