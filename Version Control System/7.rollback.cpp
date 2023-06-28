b16399e6cb3cca4e378d91bf5ec4881fe3025dca
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-06-28 10:15:38 +0530

    ..

--- Version Control System/7.rollback.cpp
+++ Version Control System/7.rollback.cpp
@@ -1,3 +1,26 @@
 namespace mygitrollback{
+    #include <iostream>
+    #include <string>
+    #include <unistd.h>
+    #include <stdio.h>
+    #include <limits.h>
+    #include <dirent.h>
+    #include <sys/types.h>
+    #include <fcntl.h>
+    #include <errno.h>
+    #include <time.h>
+    #include <vector>
+    #include <map>
+    #include <fstream>
+    #include <algorithm>
+    #include <sys/stat.h>
+    #include <sstream>
     
+    using namespace std;
+    
+    string base_path = "";
+    string cwd_path[PATH_MAX];
+    string get_cwd();
+    vector<string> get_files(string);
+    string get_sha(string);
 }
