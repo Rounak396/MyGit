50f20738322e960b49a1b8610c659306b8cc6385
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-06-29 17:55:00 +0530

    Update 9.mygit.cpp

--- Version Control System/9.mygit.cpp
+++ Version Control System/9.mygit.cpp
@@ -26,6 +26,17 @@ using namespace std;
 #include "8.status.cpp"
 
 
+using namespace std;
+using namespace mygitlog;
+using namespace mygitstatus;
+using namespace mygitadd;
+using namespace mygitCommit;
+using namespace mygitpull;
+using namespace mygitpush;
+using namespace mygitmerge;
+using namespace mygitrollback;
+
+
 int init();
 string retrieve_version_no();
 void retrieve_sha(string , string );
@@ -35,4 +46,189 @@ void retrieve_filename(string , string );
 
 int main(int argc, char* argv[]){
     
+    string str;
+    char * p=argv[1];
+    str=p;
+
+    if (str == "init"){    
+            if (init())
+            {
+                string s = "Mygit successfully initialised!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+                
+            }
+            else
+            {
+                string s = "Error in initialisation!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+                
+                
+            }
+        }
+
+    else if (str == "add")
+        {
+            if (argc == 3)
+            {
+                string s = "File added successfully!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                add(argv[2]);
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in adding file!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+    else if (str == "commit")
+        {
+            if (argc == 3)
+            {
+                string s = "Commit successful!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                commit(argv[2]);
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in commit!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+    else if (str == "log")
+        {
+            if (argc == 2)
+            {
+                string s = "Log successful!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                log();
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in log!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+    else if (str == "merge")
+        {
+            if (argc == 3)
+            {
+                string s = "Merge successful!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                merge(argv[2]);
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in merge!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+    else if (str == "pull")
+        {
+            if (argc == 3)
+            {
+                string s = "Pull successful!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                pull(argv[2]);
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in pull!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+    else if (str == "push")
+        {
+            if (argc == 3)
+            {
+                string s = "Push successful!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                push(argv[2]);
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in push!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+    else if (str == "rollback")
+        {
+            if (argc == 3)
+            {
+                string s = "Rollback successful!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                rollback(argv[2]);
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in rollback!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+    else if (str == "status")
+        {
+            if (argc == 2)
+            {
+                string s = "Status successful!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                status();
+                exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                string s = "Error in status!!";
+                mygitlog::log_write(s);
+                cout << s << endl;
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+     else
+        {
+            string s = "Invalid command!!";
+            mygitlog::log_write(s);
+            cout << s << endl;
+            exit(EXIT_SUCCESS);
+        }
+
+        
 }
