73cd733bea0463c75df552ac8a1e2b8863561723
TheCatalog360 <rs.21u10471@btech.nitdgp.ac.in>
2023-06-29 16:53:03 +0530

    Update 9.mygit.cpp

--- Version Control System/9.mygit.cpp
+++ Version Control System/9.mygit.cpp
@@ -16,3 +16,23 @@
 
 using namespace std;
 
+#include "1.add.cpp"
+#include "2.commit.cpp"
+#include "3.log.cpp"
+#include "4.merge.cpp"
+#include "5.pull.cpp"
+#include "6.push.cpp"
+#include "7.rollback.cpp"
+#include "8.status.cpp"
+
+
+int init();
+string retrieve_version_no();
+void retrieve_sha(string , string );
+void retrieve_filename(string , string );
+
+
+
+int main(int argc, char* argv[]){
+    
+}
